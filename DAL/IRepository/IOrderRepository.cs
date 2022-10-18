aea6eb4e5f6d693675a0d21d829a0b1927fc19e5
Riley Lin <75247434+FakeStandard@users.noreply.github.com>
2022-10-18 15:41:47 +0800

    add order repository

---
+++ DAL/IRepository/IOrderRepository.cs
@@ -0,0 +1,10 @@
+﻿using repository_practice.Models;
+
+namespace repository_practice.DAL.IRepository
+{
+    public interface IOrderRepository : IRepository<Order>
+    {
+        IList<Order> GetByCustomerID(int id);
+        IList<Order> GetByEmployeeID(int id);
+    }
+}
---
+++ DAL/Repository/OrderRepository.cs
@@ -0,0 +1,30 @@
+﻿using repository_practice.DAL.IRepository;
+using repository_practice.Models;
+
+namespace repository_practice.DAL.Repository
+{
+    public class OrderRepository : Repository<Order>, IOrderRepository
+    {
+        public OrderRepository(NorthwindContext context)
+            : base(context)
+        { }
+
+        public IList<Order> GetByCustomerID(int id)
+        {
+            var res = from r in context.Orders
+                      where r.CustomerID == id.ToString()
+                      select r;
+
+            return res.ToList();
+        }
+
+        public IList<Order> GetByEmployeeID(int id)
+        {
+            var res = from r in context.Orders
+                      where r.EmployeeID == id
+                      select r;
+
+            return res.ToList();
+        }
+    }
+}
