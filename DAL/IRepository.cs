6e9be66adbb8518b1db830ff4641c016589f9a8b
Riley Lin <75247434+FakeStandard@users.noreply.github.com>
2022-10-18 15:14:22 +0800

    create repository pattern

---
+++ DAL/IRepository.cs
@@ -0,0 +1,12 @@
+﻿namespace repository_practice.DAL
+{
+    public interface IRepository<T> where T : class
+    {
+        IEnumerable<T> Get();
+        T GetByID(object id);
+        void Insert(T entity);
+        void Update(T entity);
+        void Delete(object id);
+        void Save();
+    }
+}
---
+++ DAL/Repository.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace repository_practice.DAL
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        internal readonly NorthwindContext context;
+        internal readonly DbSet<T> dbSet;
+
+        public Repository(NorthwindContext context)
+        {
+            this.context = context;
+            this.dbSet = context.Set<T>();
+        }
+
+        public IEnumerable<T> Get()
+        {
+            throw new NotImplementedException();
+        }
+
+        public T GetByID(object id)
+        {
+            return dbSet.Find(id);
+        }
+
+        public void Insert(T entity)
+        {
+            dbSet.Add(entity);
+        }
+
+        public void Update(T entity)
+        {
+            dbSet.Attach(entity);
+            context.Entry(entity).State = EntityState.Modified;
+        }
+
+        public void Delete(object id)
+        {
+            Delete(GetByID(id));
+        }
+
+        protected void Delete(T entity)
+        {
+            if (context.Entry(entity).State == EntityState.Detached)
+                dbSet.Attach(entity);
+
+            dbSet.Remove(entity);
+        }
+
+        public void Save()
+        {
+            context.SaveChanges();
+        }
+    }
+}
